50bd613174d26e9b539f8211954ec766eb1a8362
N1kOk <n1k_ok@bk.ru>
2023-09-27 13:06:38 +0500

    feat: add background component

---
+++ src/components/Background.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react'
+import O from '@/components/O'
+import X from '@/components/X'
+
+const animationTime = 50
+const rnd = (min: number, max: number) => Math.round(Math.random() * (max - min) + min)
+
+const listO = [...Array(7)].map(_ => ({ left: rnd(5, 95), timeStart: -rnd(0, animationTime) }))
+const listX = [...Array(8)].map(_ => ({ rotate: rnd(-30, 30), left: rnd(5, 95), timeStart: -rnd(0, animationTime) }))
+
+const Background: FC = () => {
+  return (
+    <>
+      {listO.map(({ left, timeStart }, i) =>
+        <O
+          key={i}
+          style={{ '--animationStartTime': `${timeStart}s`, left: `${left}vw` }}
+          className={'background-shape opacity-5'}
+        />,
+      )}
+      {listX.map(({ rotate, left, timeStart }, i) =>
+        <div
+          // @ts-ignore
+          style={{ '--animationStartTime': `${timeStart}s`, left: `${left}vw` }}
+          className={'background-shape h-[25px]'}
+        >
+          <X
+            key={i}
+            style={{ transform: `rotate(${rotate}deg)` }}
+            className={'opacity-5'}
+          />
+        </div>,
+      )}
+    </>
+  )
+}
+
+export default Background
